a04804aa8c5488378a632bf974309ffcda4ff355
xpc2000 <61380068+xpc2000@users.noreply.github.com>
2020-12-03 11:03:49 +0800

    Add files via upload

--- goodsservice/src/main/java/cn/edu/xmu/goods/controller/FloatPriceController.java
+++ goodsservice/src/main/java/cn/edu/xmu/goods/controller/FloatPriceController.java
@@ -1,14 +1,17 @@
 package cn.edu.xmu.goods.controller;
 
+import cn.edu.xmu.goods.model.vo.FloatPriceRetVo;
+import cn.edu.xmu.goods.model.vo.FloatPriceVo;
 import cn.edu.xmu.goods.service.FloatPriceService;
 import cn.edu.xmu.ooad.annotation.Audit;
 import cn.edu.xmu.ooad.annotation.LoginUser;
+import cn.edu.xmu.ooad.util.Common;
 import cn.edu.xmu.ooad.util.ReturnObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * @Author：谢沛辰
  * @Date: 2020.12.01
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/shops",produces = "application/json;charset+UTF-8")
 public class FloatPriceController {
+    private  static  final Logger logger = LoggerFactory.getLogger(FloatPriceController.class);
 
     @Autowired
     FloatPriceService floatPriceService;
@@ -25,6 +29,13 @@ public class FloatPriceController {
     @DeleteMapping("/shops/{shopId}/floatPrices/{id}")
     public Object deleteFloatPrice(@LoginUser Long userId, @PathVariable Long id){
         ReturnObject<Object> result = floatPriceService.logicallyDelete(userId,id);
-        return result;
+        return Common.decorateReturnObject(result);
+    }
+
+    @Audit
+    @PostMapping("/shops/{shopId}/skus/{id}/floatPrices")
+    public Object createFloatPrice(@LoginUser Long userId, @PathVariable Long id, @RequestBody FloatPriceVo floatPriceVo){
+        ReturnObject<FloatPriceRetVo> result =floatPriceService.createFloatPrice(id,floatPriceVo,userId);
+        return Common.decorateReturnObject(result);
     }
 }
